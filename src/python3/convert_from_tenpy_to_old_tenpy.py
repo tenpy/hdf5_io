bc97f79264a83cfb4d60ed080cd62ddb28fa6781
Johannes Hauschild <git@johannes-hauschild.de>
2020-02-04 16:39:16 -0800

    convert model from tenpy to old tenpy

--- src/python3/convert_from_tenpy_to_old_tenpy.py
+++ src/python3/convert_from_tenpy_to_old_tenpy.py
@@ -115,6 +115,21 @@ class Converter(Hdf5Converter):
     mappings[('tenpy.networks.mps', 'MPS')] = \
         ('mps.mps', 'iMPS', convert_MPS)
 
+    def convert_index_identity(self, Id_LR, bc):
+        """Convert  `IdL`->`vL`and `IdR`->`vR` for an MPO."""
+        # new tenpy: bond b left  of site b, always L+1 entries
+        # old tenpy: bond b right of site b, length L+1 for segment bc, others L
+        Id_LR0, Id_LR = Id_LR[0], Id_LR[1:]  # now entry b is left of site b
+        if bc == 'infinite' or bc == 'periodic':
+            pass  # fine
+        elif bc == 'finite':
+            pass # fine
+        elif bc == 'segment':
+            Id_LR.append(Id_LR0)
+        else:
+            raise ValueError("don't recognise bc_MPS=" + repr(bc))
+        return Id_LR
+
     def convert_MPO(self, h5gr_orig, subpath_orig, h5gr_new, subpath_new):
         L = self.get_attr(h5gr_orig, "L")
         # tensors
@@ -134,12 +149,8 @@ class Converter(Hdf5Converter):
         IdR = self.load(subpath_orig + "index_identity_right")
         # new tenpy: bond b left  of site b, always L+1 entries
         # old tenpy: bond b right of site b, length L+1 for segment bc, others L
-        IdL0, IdL = IdL[0], IdL[1:]  # now entry b is left of site b
-        IdR0, IdR = IdR[0], IdR[1:]  # now entry b is left of site b
-        if bc == "segment":
-            IdL.append(IdL0)
-            IdR.append(IdR0)
-        # else: already correct L entries
+        IdL = self.convert_index_identity(IdL, bc)
+        IdR = self.convert_index_identity(IdR, bc)
         self.save(IdL, subpath_new + "index_identity_left")
         self.save(IdR, subpath_new + "index_identity_right")
 
@@ -147,11 +158,87 @@ class Converter(Hdf5Converter):
         h5gr_new.attrs["L"] = self.get_attr(h5gr_orig, "L")
         h5gr_new.attrs["max_bond_dimension"] = self.get_attr(h5gr_orig, "max_bond_dimension")
         h5gr_new.attrs["grouped"] = self.get_attr(h5gr_orig, "grouped")
-        self.save(None, subpath_new + "max_range")  # unknown
         self.save(None, subpath_new + "translate_Q1_data")
 
     mappings[('tenpy.networks.mpo', 'MPO')] = \
         ('mps.mpo', 'MPO', convert_MPO)
+
+    def convert_model(self, h5gr_orig, subpath_orig, h5gr_new, subpath_new):
+        assert h5gr_orig.attrs[hdf5_io.ATTR_FORMAT] == hdf5_io.REPR_DICT_SIMPLE
+        lat = self.load(subpath_orig + "lat")
+        sites = lat.mps_sites()
+        L = len(sites)
+        bc = lat.bc_MPS
+        self.save(self.convert_bc(bc), subpath_new + "boundary_condition")
+
+        if "H_bond" in h5gr_orig:
+            # have nearest-neighbor Hamiltonian
+            H_bond = list(self.load(subpath_orig + "H_bond"))
+            # new tenpy: always L entries, entry b is sites (b-1, b), None if not to be used.
+            # old tenpy: always L entries, entry b is sites (b, b+1), for finite: last entry 0.
+            H_bond =  H_bond[1:] + [H_bond[0]]  # now entry b is sites (b, b+1)
+            if bc == 'finite':
+                p0 = H_bond[-2].get_leg('p1')
+                H_bond[-1] = npc.zeros([p0, p1, p0.conj(), p1.conj()],
+                                       lables=['p0', 'p1', 'p0*', 'p1*'])
+            for H in H_bond:
+                H.itranspose(['p0', 'p1', 'p0*', 'p1*'])  # same labels
+            self.save(H_bond, subpath_new + "H")
+            self.convert_group(h5gr_new["H"])  # convert npc Arrays
+
+        # convert MPO directly to extract the converted tensors
+        # (conversion might also have happend before starting the conversion of the model!)
+        H_mpo_gr = self.convert_group(h5gr_orig['H_MPO'])  # convert MPO: does most of the job
+        h5gr_new["H_mpo"] = H_mpo_gr["tensors"]
+        h5gr_new["index_identity_left"] = H_mpo_gr["index_identity_left"]
+        h5gr_new["index_identity_right"] = H_mpo_gr["index_identity_right"]
+        h5gr_new["translate_Q1_data"] = H_mpo_gr["translate_Q1_data"]
+        h5gr_new.attrs['grouped'] = self.get_attr(H_mpo_gr, "grouped")
+
+        states = [s.state_labels for s in sites]
+        self.save(states, subpath_new + "states")
+        onsite_ops = {}
+        for i, s in enumerate(sites):
+            for opname in s.opnames:
+                op_list = onsite_ops.setdefault(opname, [None]*L)
+                op_list[i] = s.get_op(opname)
+        self.save(onsite_ops, subpath_new + "onsite_operators")
+        self.save({}, subpath_new + "bond_operators")  # doesn't exist (yet?) in new TeNPy
+        self.save(False, subpath_new + "add_conj")  # doesn't exist (yet?) in new TeNPy
+        h5gr_new.attrs['L'] = L
+        dims = np.array([s.dim for s in sites])
+        self.save(dims, subpath_new + "dimensions")
+
+
+    for _model in [('tenpy.models.model', 'Model'), # base class
+                   ('tenpy.models.model', 'NearestNeighborModel'), # base class
+                   ('tenpy.models.model', 'MPOModel'), # base class
+                   ('tenpy.models.model', 'CouplingMPOModel'), # base class
+                   # and derived classes defined in TeNPy
+                   # TODO: hard-coded list of models
+                   # needs to be updated if new models are implemented in TeNPy
+                   ('tenpy.models.fermions_spinless', 'FermionModel'),
+                   ('tenpy.models.fermions_spinless', 'FermionChain'),
+                   ('tenpy.models.haldane', 'BosonicHaldaneModel'),
+                   ('tenpy.models.haldane', 'FermionicHaldaneModel'),
+                   ('tenpy.models.hofstadter', 'HofstadterFermions'),
+                   ('tenpy.models.hofstadter', 'HofstadterBosons'),
+                   ('tenpy.models.hubbard', 'BoseHubbardModel'),
+                   ('tenpy.models.hubbard', 'BoseHubbardChain'),
+                   ('tenpy.models.hubbard', 'FermiHubbardModel'),
+                   ('tenpy.models.hubbard', 'FermiHubbardChain'),
+                   ('tenpy.models.spins_nnn', 'SpinChainNNN'),
+                   ('tenpy.models.spins_nnn', 'SpinChainNNN2'),
+                   ('tenpy.models.spins', 'SpinModel'),
+                   ('tenpy.models.spins', 'SpinChain'),
+                   ('tenpy.models.tf_ising', 'TFIModel'),
+                   ('tenpy.models.tf_ising', 'TFIChain'),
+                   ('tenpy.models.toric_code', 'ToricCode'),
+                   ('tenpy.models.xxz_chain', 'XXZChain'),
+                   ('tenpy.models.xxz_chain', 'XXZChain2'),
+                   ]:
+        mappings[_model] = ('models.model', 'model', convert_model)
+    del _model
 
 
 if __name__ == "__main__":
